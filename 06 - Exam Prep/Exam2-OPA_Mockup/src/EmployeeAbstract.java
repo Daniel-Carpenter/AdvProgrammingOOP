89ef26f111fb047fc192a52e7b6e37193486375e
Daniel Carpenter <dbry1024@gmail.com>
2020-11-10 09:50:40 -0600

    Abstract class for employee

---
+++ 06 - Exam Prep/Exam2-OPA_Mockup/src/EmployeeAbstract.java
@@ -0,0 +1,10 @@
+
+/**
+ * @author Daniel Carpenter
+ */
+public abstract class EmployeeAbstract {
+	abstract void setEmpID();
+	abstract void setBaseSalary(int salary);
+	public void setStatus(EmployeeStatus status) {
+	}
+}
