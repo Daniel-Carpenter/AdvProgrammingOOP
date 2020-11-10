4e0e42dc2f0323419677e820885c8930c8c08ec0
Daniel Carpenter <dbry1024@gmail.com>
2020-11-10 09:51:17 -0600

    First draft of Employee class

---
+++ 06 - Exam Prep/Exam2-OPA_Mockup/src/Employee.java
@@ -0,0 +1,41 @@
+
+public class Employee extends EmployeeAbstract {
+
+	private static int empID = 0;
+	private int baseSalary;
+	private EmployeeStatus status;
+
+	public Employee(int baseSalary, EmployeeStatus status) {
+		setEmpID();
+		setBaseSalary(baseSalary);
+		setStatus(status);
+	}
+
+	@Override
+	public void setEmpID() {
+		++empID;
+	}
+
+	@Override
+	public void setBaseSalary(int baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+
+	@Override
+	public void setStatus(EmployeeStatus status) {
+		this.status = status;
+
+	}
+
+	public int getEmpID() {
+		return empID;
+	}
+
+	public int getBaseSalary() {
+		return baseSalary;
+	}
+
+	public EmployeeStatus getStatus() {
+		return status;
+	}
+}
